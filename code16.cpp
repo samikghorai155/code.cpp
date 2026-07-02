473feb732b0a2aa9fc683880dee0f3aeb218fd61
samik ghorai <samikghorai155@gmail.com>
2026-07-02 22:26:07 +0530

    binary to decimel

---
+++ code16.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string binary;
+    cin >> binary;
+
+    int decimal = 0;
+
+    for (char bit : binary) {
+        decimal = decimal * 2 + (bit - '0');
+    }
+
+    cout << "Decimal = " << decimal << endl;
+    return 0;
+}
