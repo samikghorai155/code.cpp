a42dc0157c3d8d67110e9a68b3d0e3c020ea8f49
samik ghorai <samikghorai155@gmail.com>
2026-09-03 22:41:18 +0530

    Add code19

---
+++ code19.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int binary, decimal = 0, base = 1, rem;
+
+    cout << "Enter a binary number: ";
+    cin >> binary;
+
+    while (binary > 0) {
+        rem = binary % 10;
+        decimal = decimal + rem * base;
+        binary = binary / 10;
+        base = base * 2;
+    }
+
+    cout << "Decimal number = " << decimal;
+
+    return 0;
+}
